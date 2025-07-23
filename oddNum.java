e8b58ef4a775cd5940b64fb754752022fc35e942
Aleena R <aleenarachelmanoj@gmail.com>
2025-07-23 10:29:47 +0530

    Add files via upload

---
+++ oddNum.java
@@ -0,0 +1,16 @@
+package labExp;
+
+public class oddNum {
+	public static void main(String [] args) {
+		int num=50;
+		for (int i=0;i<50;i++) {
+			if(i%2==0) {
+			continue;
+			}
+			else {
+
+				System.out.println(i);
+			}
+		}
+	}
+}
